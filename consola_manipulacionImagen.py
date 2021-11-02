2ef2fad23c195b90777274d299229763d61396d5
Oscar Betancourt <oi.betancourt10@uniandes.edu.co>
2021-11-01 19:26:07 -0500

    Primero

---
+++ consola_manipulacionImagen.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 17:50:26 2021
+
+@author: oibm
+"""
+
+import manipulacionImagen as mi
+from PIL import Image
+from pymongo import MongoClient
+     
+uri = "mongodb+srv://Oscar:1234@appimagenes.uvok1.mongodb.net/AppImagenes?retryWrites=true&w=majority"
+cliente = MongoClient(uri)
+AppImagenes = cliente["AppImagenes"]
+Lista_Fotos = AppImagenes["Lista_Fotos"]
+Lista_Fotos_Modificadas = AppImagenes["Lista_Fotos_Modificadas"]
+
+def ejecutar_cargar_imagen(ubicacion:str):
+    """
+    Descripción:
+    Ejecuta la función que carga la imagen solicitada en una Colección determinada.
+    Parametros:
+    -ubicacion(str): Nombre de la Colección donde se encuentra la información de la imagen a guardar. 
+    Salidas:
+    NA
+    Errores:
+    -El nombre tiene que incluir la extención adecuada. 
+    """
+    try:
+        cantidad_imagenes = int(input("\nCuántas imágenes desea agregar? "))
+        while cantidad_imagenes > 0:
+            
+            nombre_imagen = input("\nIngrese el nombre de la imagen con la extención JPG: ")
+            try:
+                Image.open("Imagenes/"+nombre_imagen)
+                mi.cargar_imagen(nombre_imagen,ubicacion)
+                print("\nLa imagen fue cargada correctamente.")
+                cantidad_imagenes -= 1
+            
+            except:
+                print("\nIngrese un nombre válido.")
+    except:
+        print("\nIngrese un numero válido")
+
+def ejecutar_mostrar_basedatos(ubicacion:str)->None:
+    """  
+    Descripción:
+    Ejecuta la función para mostrar el listado de objetos de una Colección determinada dentro de la base de datos. 
+    Parámetros:
+    -ubicacion(str): Nombre de la Colección donde se encuentra la información de la imagenes para ser mostradas en una lista. 
+    Salidas:
+    -NA
+    Errores:
+    NA
+    """
+    mi.mostrar_basedatos(ubicacion)
+
+def ejecutar_seleccionar_imagen(imagen_seleccionada:int, ubicacion:str)->Image:
+    """
+    Descripción:
+    Ejecuta la función que retorna la imagen que desea ser mostrada en pantalla. 
+    Parámetros:
+    -imagen_seleccionada(int): Indice de la imagen que desea ser abierta en una Colección determinada.
+    -ubicacion(str): Nombre de la Colección donde se encuentra la información de la imagen a mostrar. 
+    Salidas:
+    -imagen(Image): Imagen que desea ser abierta.
+    Errores:
+    NA
+    """
+    imagen = mi.seleccionar_imagen(imagen_seleccionada,ubicacion)
+    return imagen
+
+def ejecutar_mostrar_imagen(ubicacion:str)->None:
+    """
+    Descripción:
+    Ejecuta la función que muestra la imagen que entra por parámetro
+    Parametros:
+    -imagen(Image): Imagen cargada
+    Salidas:
+    NA
+    Errores:
+    NA
+    """
+    indice, objetoLista, directorio = mi.info_listas(ubicacion)
+
+    indice = objetoLista.estimated_document_count()
+
+    if indice > 0:
+        ejecutar_mostrar_basedatos(ubicacion)
+        completado = False
+        while completado == False:
+            try:
+                imagen_seleccionada = int(input("\nSeleccione la imagen a mostrar: "))
+                imagen = ejecutar_seleccionar_imagen(imagen_seleccionada,ubicacion)
+                mi.mostrar_imagen(imagen)
+                completado = True 
+            except:
+                print("\nIngrese un numero válido.")
+    else:
+        if ubicacion == "Lista_Fotos":
+            print("\nCargue una imagen para poder realizar esta acción.")
+        elif ubicacion == "Lista_Fotos_Modificadas":
+            print("\nProcese una imagen y guardela para poder realizar esta acción.")
+    
+def ejecutar_procesar_imagen():
+    """
+    Descripción:
+    Ejecuta la función que procesa la imagen que entra por parámetro.
+    Parametros:
+    -imagen(Image): Imagen cargada
+    Salidas:
+    -imagen(Image): Imagen que desea ser abierta.
+    Errores:
+    NA
+    """
+    indice = Lista_Fotos.estimated_document_count()
+    if indice > 0:
+        mi.mostrar_basedatos("Lista_Fotos")
+        completado = False
+        while completado == False:
+            try:
+                imagen_seleccionada = int(input("\nSeleccionar la imagen a procesar: "))
+                imagen = ejecutar_seleccionar_imagen(imagen_seleccionada,"Lista_Fotos")
+                imagen = mi.procesar_imagen(imagen)
+                print("\n1. Si")
+                print("2. No")
+                guardar = input("\nDesea guardar la imagen?: ")
+                if guardar == "1":
+                    nombreValido = False
+                    while nombreValido == False:
+                        try:
+                            nombre_imagen = input("\nNombre de la imagen con la extensión (JPG): ")
+                            mi.guardar_imagen(imagen, nombre_imagen, "Lista_Fotos_Modificadas")
+                            nombreValido = True
+                            print("\nLa imagen fue cargada correctamente.")
+                        except:
+                            print("\nIngrese un nombre válido.")
+                completado = True
+            except:
+                print("\nIngrese un numero válido.")
+    else:
+        print("\nCargue una imagen para poder realizar esta acción.")
+    
+def ejecutar_eliminar_imagen()->None:
+    """
+    Descripción:
+    Ejecuta la función que elimina la imagen de la base de datos y de la carpeta donde se encuentra guardada. 
+    Parámetros:
+    -imagen_seleccionada(int): Valor entero con el índice de la imagen que el usuario desea eliminar de una Colección determinada.
+    -ubicacion(str): Nombre de la Colección donde se encuentra la información de la imagen a guardar. 
+    Salidas:
+    NA
+    Errores:
+    NA  
+    """
+    numeroValido = False
+    while numeroValido == False:
+        try:    
+            print("\n1. Lista_Fotos")
+            print("2. Lista_Fotos_Modificadas")
+            ubicacion = input("\nEscoja la ubicación: ")
+            if ubicacion == "1":
+                ubicacion = "Lista_Fotos"
+                indice = Lista_Fotos.estimated_document_count()
+            elif ubicacion == "2":
+                ubicacion = "Lista_Fotos_Modificadas"
+                indice = Lista_Fotos_Modificadas.estimated_document_count()
+            if indice > 0:
+                ejecutar_mostrar_basedatos(ubicacion)
+                eliminado = False
+                while eliminado == False:
+                    try:
+                        imagen_seleccionada = int(input("\nSeleccionar la imagen a eliminar: "))
+                        mi.eliminar_imagen(imagen_seleccionada, ubicacion)
+                        print("\nLa imagen fue eliminada correctamente.")
+                        eliminado = True
+                    except:
+                        print("\nIngrese un numero válido.")
+            else:
+                print("\nCargue y/o procese una imagen para poder realizar esta acción.")
+            numeroValido = True
+        except:
+            print("\nIngrese un numero válido")
+
+
+def menu()->None:
+    """
+    Descripción:
+    Función que determina el menú a mostrar en la consola.
+    CRUD (Create, Read, Update, Delete)
+    Parametros:
+    NA
+    Salidas:
+    NA
+    Errores:
+    NA
+    """
+    print("\nOPCIONES\n")
+    print("0. Cargar Imagen")
+    print("1. Mostrar Imagen")
+    print("2. Procesar Imagen")
+    print("3. Mostrar Imagen Procesada")
+    print("4. Eliminar Imagen")
+    print("5. Salir\n")
+
+def iniciar_aplicacion()->None:
+    """
+    Descripción:
+    Función para iniciar la aplicación.
+    Ejecuta el menú y solicita un numero para llevar a cabo una acción determinada. 
+    Parametros:
+    NA
+    Salidas:
+    NA
+    Errores:
+    -Intentar ejecutar una acción sin cargar previamente una imagen. 
+    -Ingresar un nombre incorrecto.
+    """
+    continuar = True
+    while continuar:
+        menu()
+        elegido = input("\nSeleccione una opcion del menu: ")
+
+        if elegido == "0":
+            ejecutar_cargar_imagen("Lista_Fotos")
+    
+        elif elegido == "1":
+            ejecutar_mostrar_imagen("Lista_Fotos")
+                
+        elif elegido == "2":
+            ejecutar_procesar_imagen()
+        
+        elif elegido == "3":
+            ejecutar_mostrar_imagen("Lista_Fotos_Modificadas")
+                        
+        elif elegido == "4":
+            ejecutar_eliminar_imagen()
+
+        elif elegido == "5":
+            continuar = False
+        
+        else:
+            print("Seleccione una opcion del menu:")
+            
+iniciar_aplicacion()
---
+++ manipulacionImagen.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 16:25:20 2021
+
+@author: oibm
+"""
+from PIL import Image
+from pymongo import MongoClient
+import os
+
+uri = "mongodb+srv://Oscar:1234@appimagenes.uvok1.mongodb.net/AppImagenes?retryWrites=true&w=majority"
+cliente = MongoClient(uri)
+AppImagenes = cliente["AppImagenes"]
+
+def info_listas(ubicacion:str):
+    """
+    Descripción:
+    Función que accede a determinados valores de las Colecciones creadas en la Base de datos.
+    Parámetros:
+    -ubicacion(str): Nombre de la carpeta que contiene las imágenes a usar <<Imagenes/>> o la carpeta donde se almacenarán 
+    las imágenes modificadas <<Imagenes_modificadas>>. Representan cada una las Colecciones usadas en la Base de Datos.
+    Salidas:
+    -indice(int): Valor entero con la cantidad actual de documentos dentro de una Colección determinada.
+    -objetoLista(collection): Colección asociada a la ubicación dada por parámetro.
+    -directorio(str): Nombre de la dirección donde se ubican las fotos. 
+    """
+    if ubicacion == "Lista_Fotos":
+        objetoLista = AppImagenes["Lista_Fotos"]
+        directorio = "Imagenes/"
+
+    if ubicacion == "Lista_Fotos_Modificadas":
+        objetoLista = AppImagenes["Lista_Fotos_Modificadas"]
+        directorio = "Imagenes_modificadas/"
+
+    indice = objetoLista.estimated_document_count()
+
+    return indice, objetoLista, directorio
+
+def cargar_imagen(nombre_imagen:str, ubicacion:str)->None:
+    """ 
+    Descripción:
+    Función que accede a la base de datos en la nube en mongodb para insertar la información de una nueva imagen en una determinada Colección. 
+    Parámetros:
+    -nombre_imagen(str): Nombre de la foto
+    -ubicacion(str): Nombre de la carpeta que contiene las imágenes a usar <<Imagenes/>> o la carpeta donde se almacenarán 
+    las imágenes modificadas <<Imagenes_modificadas>>. Representan cada una las Colecciones usadas en la Base de Datos.
+    Salidas:
+    NA
+    Errores:
+    NA
+    """ 
+    indice, objetoLista, directorio = info_listas(ubicacion)
+    datosFoto = {"id":indice+1, "Nombre imagen":nombre_imagen}
+    objetoLista.insert_one(datosFoto)
+
+def mostrar_basedatos(ubicacion:str):
+    """
+    Descripción:
+    Función que despliega la lista de fotos guardadas en una determinada Colección de la base de datos de Mongodb.
+    Parámetros:
+    -ubicacion(str): Nombre de la carpeta que contiene las imágenes a usar <<Imagenes/>> o la carpeta donde se almacenarán 
+    las imágenes modificadas <<Imagenes_modificadas>>. Representan cada una las Colecciones usadas en la Base de Datos.
+    Salidas:
+    NA
+    Errores:
+    Na
+    """    
+    indice, objetoLista, directorio = info_listas(ubicacion)
+    print("")
+    for i in objetoLista.find():
+        print(i["id"]," ",i["Nombre imagen"])
+    
+
+def seleccionar_imagen(imagen_seleccionada:int, ubicacion:str)->Image:
+    """
+    Descripción:
+    Función que retorna la imagen que desea ser mostrada en pantalla. 
+    Parametros:
+    -imagen_seleccionada(int): Indice de la imagen que desea ser abierta en una Colección determinada.
+    -ubicacion(str): Nombre de la Colección donde se encuentra la información de la imagen a mostrar. 
+    Salidas:
+    -imagen(Image): Imagen que desea ser abierta.
+    Errores:
+    NA
+    """
+    indice, objetoLista, directorio = info_listas(ubicacion)
+
+    imagen = objetoLista.find_one({"id":imagen_seleccionada})
+    nombre_imagen = directorio + imagen["Nombre imagen"]
+    imagen = Image.open(nombre_imagen)
+
+    return imagen
+
+def mostrar_imagen(imagen:Image)->None:
+    """
+    Descripción:
+    Función que muestra la imagen que entra por parámetro con un visualizador externo.
+    Parametros:
+    -imagen(Image): Imagen cargada previamente. 
+    Salidas:
+    NA
+    Errores:
+    NA
+    """
+    imagen.show()
+    
+def encontrar_tamano(orientacion:str, hojaA4:tuple, ancho:int, alto:int)-> tuple:
+    """
+    Descripción:
+    Función que calcula las nuevas dimensiones de la imagen requeridos para entrar en una hoja tamaño A4. Las nuevas dimensiones son calculadas con un error
+    de 0.5%
+    Restricciones:
+    -La imagen no puede perder su ratio.
+    -Se debe aprovechar el máximo de la hoja A4.
+    -Ninguna imagen debe ser agrandada en el proceso, solo encogida cuando corresponda.
+    -La orientación de la página se debe definir a partir de la orientación de la imagen.
+    Parametros:
+    -orientación(str): Orientación de la imagen cargada.
+    -hojaA4(tuple): Una tupla con el tamaño de la hoja A4 respecto a la orientación requerida. (Ancho, Alto)
+    -ancho(int): Ancho de la imagen cargada.
+    -alto(int): Alto de la imagen cargada.
+    Salidas:
+    -tamaño(tuple): Una tupla con el nuevo tamaño de la imagen con un error de 0.5%. 
+    Errores:
+    NA
+    """
+    
+    ratio = round((max(ancho,alto)/min(ancho,alto)),2)
+    error = 0.005
+    topeMax = ratio*(1+error)
+    topeMin = ratio*(1-error)
+    
+    i = 1
+    j = 1
+    areaMinima = 100000*100000
+    
+    for i in range(1,hojaA4[0]):
+        for j in range(1,hojaA4[1]):
+            nuevoRatio = round((max(i,j)/min(i,j)),2)
+            area = hojaA4[0]*hojaA4[1] - i*j
+            if nuevoRatio <= topeMax and nuevoRatio >= topeMin:
+                
+                areaMinima = min(areaMinima,area)
+                
+                if orientacion == "Vertical":
+                    ancho = min(i,j)
+                    alto = max(i,j)
+    
+                if orientacion == "Horizontal":
+                    ancho = max(i,j)
+                    alto = min(i,j)
+    
+    tamaño = (ancho,alto)
+    return tamaño        
+
+def procesar_imagen(imagen:Image)->Image:
+    """
+    Descripción:
+    Función que procesa la imagen que entra por parámetro con la función "encontrar_tamano".
+    Parametros:
+    -imagen(Image): Imagen cargada previamente. 
+    Salidas:
+    -nuevaImagen(Image): Imagen con las nuevas dimensiones de acuerdo a las restricciones del problema. 
+    Errores:
+    NA
+    """
+    
+    ancho = imagen.width
+    alto = imagen.height
+    
+    mensaje = "\nLa imagen no fue modificada.\n"
+    
+    tamaño = (ancho,alto)
+    
+    orientacion = None
+    if alto > ancho:
+        orientacion = "Vertical"
+        hojaA4 = (796,1123)
+        
+    elif alto <= ancho:
+        orientacion = "Horizontal"
+        hojaA4 = (1123,796)
+    
+    if hojaA4[0] <= ancho or hojaA4[1] <= alto:
+        tamaño = encontrar_tamano(orientacion, hojaA4, ancho, alto)
+        mensaje = "\nLas nuevas dimensiones de la imagen son: \n\nAncho: " + str(tamaño[0]) + " pixeles" + "\nAlto: " + str(tamaño[1]) + " pixeles"
+    
+    nuevaImagen = imagen.resize(tamaño)
+    
+    nuevaImagen.show()
+    
+    print(mensaje)
+    
+    return nuevaImagen
+
+def guardar_imagen(imagen:Image, nombre_imagen:str,ubicacion:str):
+    """  
+    Descripción:
+    Función que guarda la imagen modificada en la carpeta para tal fin y que guarda su información en la base de datos en la Colección adecuada. 
+    Parámetros:
+    -imagen(Image): Imagen a guardar
+    -nombre_imagen(str): Nombre que el usuario ingresa para guardar la imagen. 
+    -ubicacion(str): Nombre de la Colección donde se encuentra la información de la imagen a guardar. 
+    Salidas:
+    NA
+    Errores:
+    NA
+    """
+    
+    imagen.save("Imagenes_modificadas/"+nombre_imagen)
+    cargar_imagen(nombre_imagen,ubicacion)
+
+def eliminar_imagen(imagen_seleccionada:int, ubicacion:str)->None:
+    """ 
+    Descripción:
+    Función que elimina la imagen de la base de datos y de la carpeta donde se encuentra guardada. 
+    Parámetros:
+    -imagen_seleccionada(int): Valor entero con el índice de la imagen que el usuario desea eliminar de una Colección determinada.
+    -ubicacion(str): Nombre de la Colección donde se encuentra la información de la imagen a guardar. 
+    Salidas:
+    NA
+    Errores:
+    NA
+    """    
+    indice, objetoLista, directorio = info_listas(ubicacion)
+    imagen = objetoLista.find_one({"id":imagen_seleccionada})
+
+    for i in range(imagen_seleccionada+1, indice+1):
+        objetoLista.update_one({"id":i},{"$inc":{"id":-1}})
+    
+    objetoLista.delete_one({"_id":imagen["_id"]})
+    os.remove(directorio+imagen["Nombre imagen"])
